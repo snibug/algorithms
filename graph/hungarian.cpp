1f9009d9a29816f3687e8059684d66f4fa925939
ChanMin Kim <kcm1700@gmail.com>
2014-05-06 00:45:14 +0900

    hungarian method 사용하기 편한 형태로 수정

--- graph/hungarian.cpp
+++ graph/hungarian.cpp
@@ -4,77 +4,92 @@
 
 using namespace std;
 
-int n, match[190], matched[190];
-int dat[190][190];
-int q[190], v[190], vcnt;
-int f[190], reach[190], reach2[190], rcnt;
+namespace hung
+{
+	// input: n, dat(which is NOT const)
+	// output: call solve(), match, matched
+	// minimum matching 계산이다.
+	const int MAX_N = 190;
 
-int augment(int r){
-	int h,t=0;
-	v[r] = ++vcnt;
-	q[t++] = r;
-	for(h=0;h<t;h++){
-		int qh = q[h];
-		for(int j=0;j<n;j++) {
-			if (dat[qh][j] != 0) continue;
-			int next = matched[j];
-			if (next == -1) {
-				for(;;){
-					int org = match[qh];
-					match[qh] = j; matched[j] = qh;
-					if(qh==r) return 1;
-					qh=f[qh]; j = org;
+	int n, match[MAX_N], matched[MAX_N];
+	int dat[MAX_N][MAX_N];
+	int q[MAX_N], v[MAX_N], vcnt;
+	int f[MAX_N], reach[MAX_N], reach2[MAX_N], rcnt;
+
+	int augment(int r){
+		int h,t=0;
+		v[r] = ++vcnt;
+		q[t++] = r;
+		for(h=0;h<t;h++){
+			int qh = q[h];
+			for(int j=0;j<n;j++) {
+				if (dat[qh][j] != 0) continue;
+				int next = matched[j];
+				if (next == -1) {
+					for(;;){
+						int org = match[qh];
+						match[qh] = j; matched[j] = qh;
+						if(qh==r) return 1;
+						qh=f[qh]; j = org;
+					}
+				} else if(v[next] != vcnt) {
+					v[next] = vcnt, f[next] = qh, q[t++] = next;
 				}
-			} else if(v[next] != vcnt) {
-				v[next] = vcnt, f[next] = qh, q[t++] = next;
 			}
 		}
+		for(int i=0;i<n;i++)
+			if (v[i] == vcnt) {
+				reach[i] = rcnt;
+				if (i != r) reach2[match[i]] = rcnt;
+			}
+		return 0;
 	}
-	for(int i=0;i<n;i++)
-		if (v[i] == vcnt) {
-			reach[i] = rcnt;
-			if (i != r) reach2[match[i]] = rcnt;
+
+	int solve() {
+		int ans = 0;
+		for(int i=0;i<n;i++) match[i] = matched[i] = -1;
+		for(int i=0;i<n;i++) {
+			int minv = *min_element(dat[i],dat[i]+n);
+			for(int j = 0;j < n;j++) dat[i][j] -= minv;
+			ans += minv;
+			minv = INT_MAX;
+			for(int j=0;j<n;j++) minv = min(minv, dat[j][i]);
+			for(int j=0;j<n;j++) dat[j][i] -= minv;
+			ans += minv;
 		}
-	return 0;
+		for(;;) {
+			++rcnt;
+			bool needMore = false;
+			for(int i=0;i<n;i++) {
+				if (match[i] >= 0) continue;
+				if (!augment(i)) needMore = true;
+			}
+			if(!needMore) break;
+			int minv = INT_MAX;
+			for(int i=0;i<n;i++) {
+				if (reach[i]!=rcnt) continue;
+				for(int j=0;j<n; j++) {
+					if (reach2[j]==rcnt) continue;
+					minv = min(minv, dat[i][j]);
+				}
+			}
+			for(int i=0;i<n;i++) {
+				if (match[i]<0) ans += minv;
+				for(int j=0;j<n;j++) {
+					if (reach[i]!=rcnt) dat[i][j] += minv;
+					if (reach2[j]!=rcnt) dat[i][j] -= minv;
+				}
+			}
+		}
+		return ans;
+	}
 }
+
 int main() {
 	int ans = 0;
-	scanf("%d",&n);
-	for(int i=0;i<n;i++) for(int j=0;j<n;j++) scanf("%d",&dat[i][j]);
-	for(int i=0;i<n;i++) match[i] = matched[i] = -1;
-	for(int i=0;i<n;i++) {
-		int minv = *min_element(dat[i],dat[i]+n);
-		for(int j = 0;j < n;j++) dat[i][j] -= minv;
-		ans += minv;
-		minv = INT_MAX;
-		for(int j=0;j<n;j++) minv = min(minv, dat[j][i]);
-		for(int j=0;j<n;j++) dat[j][i] -= minv;
-		ans += minv;
-	}
-	for(;;) {
-		++rcnt;
-		bool needMore = false;
-		for(int i=0;i<n;i++) {
-			if (match[i] >= 0) continue;
-			if (!augment(i)) needMore = true;
-		}
-		if(!needMore) break;
-		int minv = INT_MAX;
-		for(int i=0;i<n;i++) {
-			if (reach[i]!=rcnt) continue;
-			for(int j=0;j<n; j++) {
-				if (reach2[j]==rcnt) continue;
-				minv = min(minv, dat[i][j]);
-			}
-		}
-		for(int i=0;i<n;i++) {
-			if (match[i]<0) ans += minv;
-			for(int j=0;j<n;j++) {
-				if (reach[i]!=rcnt) dat[i][j] += minv;
-				if (reach2[j]!=rcnt) dat[i][j] -= minv;
-			}
-		}
-	}
-	printf("%d\n",ans);
+	scanf("%d",&hung::n);
+	int n = hung::n;
+	for(int i=0;i<n;i++) for(int j=0;j<n;j++) scanf("%d",&hung::dat[i][j]);
+	printf("%d", hung::solve());
 	return 0;
 }
