4cd35f67f6010812442e33b959fefdb2220959fc
ChanMin Kim <kcm1700@gmail.com>
2014-07-16 08:35:07 +0900

    add circle related formulas

--- geometry/simple.cpp
+++ geometry/simple.cpp
@@ -1,4 +1,52 @@
-void rotate_angle(double &x, double &y, double angle) {
+#include <cmath>
+
+using namespace std;
+
+const double eps = 1e-9;
+const double PI = atan(1.0)*4;
+
+struct circle
+{
+	double x, y, r;
+};
+
+double getCenterDist(const circle &a, const circle &b) {
+	double dx = a.x - b.x;
+	double dy = a.y - b.y;
+	return sqrt(dx*dx + dy*dy);
+}
+
+
+bool isIn(const circle &a, const circle &b) {
+	double d = getCenterDist(a,b);
+	return d + a.r <= b.r + eps;
+}
+
+// obtained by Green's theorem
+// integral_{C} (L dx + M dy) = doubleintegral_{D} [partial_x(M) - partial_y(L) dx dy]
+//
+//  1
+// --- integral_{C} [x dy - y dx]
+//  2
+//
+//  x = x_c + r_c cos t
+//  y = y_c + r_c sin t
+//
+//  t parametrization
+//  dx = -r_c sin t dt
+//  dy =  r_c cos t dt
+double getLineIntegral(double x, double y, double r, double th1, double th2) {
+	double ds = sin(th2) - sin(th1);
+	double dc = cos(th2) - cos(th1);
+	return 0.5 * ( r*(x*ds-y*dc) + r*r*(th2 - th1));
+}
+
+double rangeAngle(double a, double b, double d) {
+	return acos((d*d+a*a-b*b)/(2*a*d));
+}
+
+
+void rotateAngle(double &x, double &y, double angle) {
 	double nx = x*cos(angle) - y*sin(angle);
 	double ny = x*sin(angle) + y*cos(angle);
 	x = nx, y = ny;
