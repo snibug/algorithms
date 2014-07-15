86c8e04f4e2ec768ea34868388ee56f10edd4ed0
ChanMin Kim <kcm1700@gmail.com>
2014-07-16 05:34:35 +0900

    mcmf nonnegative version

---
+++ graph/mcmf.cpp
@@ -0,0 +1,104 @@
+#include <vector>
+#include <memory.h>
+#include <limits>
+#include <algorithm>
+#include <functional>
+#include <queue>
+
+using namespace std;
+
+// without negative cost
+namespace MCMF
+{
+	typedef int cap_t;
+	typedef int cost_t;
+
+	bool isZeroCap(cap_t cap) {
+		return cap == 0;
+	}
+
+	const cap_t CAP_MAX = numeric_limits<cap_t>::max();
+	const cost_t COST_MAX = numeric_limits<cost_t>::max();
+
+	struct edge_t {
+		int target;
+		cap_t cap;
+		cost_t cost;
+		int rev;
+	};
+
+	int n;
+	vector<vector<edge_t>> graph;
+	vector<cost_t> pi;
+
+	vector<cost_t> dist;
+	vector<cap_t> mincap;
+	vector<int> from, v;
+
+	void init(int _n) {
+		n = _n;
+		graph.clear(); graph.resize(n);
+		pi.clear(); pi.resize(n);
+	}
+
+	void addEdge(int a, int b, cap_t cap, cost_t cost) {
+		edge_t forward = {b, cap, cost, (int)graph[b].size()};
+		edge_t backward = {a, 0, -cost, (int)graph[a].size()};
+		graph[a].push_back(forward);
+		graph[b].push_back(backward);
+	}
+
+	bool dijkstra(int s, int t) {
+		typedef pair<cost_t, int> pq_t;
+		priority_queue<pq_t, vector<pq_t>, greater<pq_t>> pq;
+		dist.resize(n); mincap.resize(n); from.resize(n); v.resize(n);
+		fill(dist.begin(),dist.end(),COST_MAX);
+		memset(&from[0], -1, n*sizeof(from[0]));
+		memset(&v[0], 0, n*sizeof(v[0]));
+		dist[s] = 0;
+		mincap[s] = CAP_MAX;
+		pq.emplace(dist[s], s);
+		while(!pq.empty()) {
+			int cur = pq.top().second; pq.pop();
+			if (v[cur]) continue;
+			v[cur] = 1;
+			for (const auto &edge : graph[cur]) {
+				int next = edge.target;
+				if (v[next]) continue;
+				if (isZeroCap(edge.cap)) continue;
+				cost_t potCost = dist[cur] + edge.cost - pi[next] + pi[cur];
+				if (dist[next] <= potCost) continue;
+				dist[next] = potCost;
+				mincap[next] = min(mincap[cur], edge.cap);
+				from[next] = edge.rev;
+				pq.emplace(dist[next], next);
+			}
+		}
+		if (dist[t] == COST_MAX) return false;
+		for(int i = 0; i < n; i++) {
+			if (dist[i] == COST_MAX) continue;
+			pi[i] += dist[i];
+		}
+		return true;
+	}
+
+	pair<cap_t, cost_t> solve(int source, int sink) {
+		cap_t total_flow = 0;
+		cost_t total_cost = 0;
+
+		while(dijkstra(source, sink)) {
+			cap_t f = mincap[sink];
+			total_flow += f;
+			for(int p = sink; p != source; ) {
+				auto &backward = graph[p][from[p]];
+				auto &forward = graph[backward.target][backward.rev];
+				forward.cap -= f;
+				backward.cap += f;
+				total_cost += forward.cost * f;
+				p = backward.target;
+			}
+		}
+
+		return make_pair(total_flow, total_cost);
+	}
+}
