639b8a06c48016548163baef15f4fea86739886a
ChanMin Kim <kcm1700@gmail.com>
2015-10-24 03:19:36 +0900

    min cost flow: removed max flow restriction, automatic normalization

--- graph/mcmf.cpp
+++ graph/mcmf.cpp
@@ -1,9 +1,8 @@
-#include <vector>
-#include <memory.h>
-#include <limits>
-#include <algorithm>
 #include <functional>
 #include <queue>
+#include <limits>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,138 +10,135 @@ using namespace std;
 // Kaka's matrix travel (POJ)
 // https://www.acmicpc.net/problem/8992
 // SRM 506 mid
+// ACM-ICPC World Finals 2015 C Catering
 
 // precondition: there is no negative cycle.
 // usage:
-//  MCMF::init(n);
-//  for(each edges) MCMF::addEdge(from, to, capacity, cost);
-//  if (negative cost edges were added) MCMF::normalizeNegativeEdges(source);
-//  MCMF::solve(source, sink);
-namespace MCMF
+//  MinCostFlow mcf(n);
+//  for(each edges) mcf.addEdge(from, to, cost, capacity);
+//  mcf.solve(source, sink); // min cost max flow
+//  mcf.solve(source, sink, 0); // min cost flow
+//  mcf.solve(source, sink, goal_flow); // min cost flow with total_flow >= goal_flow if possible
+struct MinCostFlow
 {
 	typedef int cap_t;
 	typedef int cost_t;
 
-	bool isZeroCap(cap_t cap) {
-		return cap == 0;
-	}
+	bool iszerocap(cap_t cap) { return cap == 0; }
 
-	const cap_t CAP_MAX = numeric_limits<cap_t>::max();
-	const cost_t COST_MAX = numeric_limits<cost_t>::max();
-
-	struct edge_t {
+	struct edge {
 		int target;
-		cap_t cap;
 		cost_t cost;
-		int rev;
+		cap_t residual_capacity;
+		cap_t orig_capacity;
+		size_t revid;
 	};
 
 	int n;
-	vector<vector<edge_t>> graph;
+	vector<vector<edge>> graph;
 	vector<cost_t> pi;
+	bool needNormalize, ranbefore;
+	int lastStart;
 
-	vector<cost_t> dist;
-	vector<cap_t> mincap;
-	vector<int> from, v;
-
-	void init(int _n) {
-		n = _n;
-		graph.clear(); graph.resize(n);
-		pi.clear(); pi.resize(n);
-
-		dist.resize(n);
-		mincap.resize(n);
-		from.resize(n);
-		v.resize(n);
+	MinCostFlow(int n) : graph(n), n(n), pi(n, 0), needNormalize(false), ranbefore(false) {}
+	void addEdge(int s, int e, cost_t cost, cap_t cap)
+	{
+		if (s == e) return;
+		edge forward={e, cost, cap, cap, graph[e].size()};
+		edge backward={s, -cost, 0, 0, graph[s].size()};
+		if (cost < 0 || ranbefore) needNormalize = true;
+		graph[s].emplace_back(forward);
+		graph[e].emplace_back(backward);
 	}
-
-	void addEdge(int a, int b, cap_t cap, cost_t cost) {
-		edge_t forward = {b, cap, cost, (int)graph[b].size()};
-		edge_t backward = {a, 0, -cost, (int)graph[a].size()};
-		graph[a].push_back(forward);
-		graph[b].push_back(backward);
-	}
-
-	// returns false if there is a negative cycle.
-	bool normalizeNegativeEdges(int s){
-		fill(dist.begin(),dist.end(),COST_MAX);
-		memset(&v[0], 0, n*sizeof(v[0]));
+	bool normalize(int s) {
+		auto infinite_cost = numeric_limits<cost_t>::max();
+		vector<cost_t> dist(n, infinite_cost);
 		dist[s] = 0;
 		queue<int> q;
-		vector<int> relaxed(n);
+		vector<int> v(n), relax_count(n);
 		v[s] = 1; q.push(s);
 		while(!q.empty()) {
 			int cur = q.front();
 			v[cur] = 0; q.pop();
-			if (++relaxed[cur] >= n) return false;
-			for(const auto &edge : graph[cur]) {
-				if (isZeroCap(edge.cap)) continue;
-				int next = edge.target;
-				cost_t nextDist = dist[cur] + edge.cost;
-				if (dist[next] > nextDist) {
-					dist[next] = nextDist;
+			if (++relax_count[cur] >= n) return false;
+			for (const auto &e : graph[cur]) {
+				if (iszerocap(e.residual_capacity)) continue;
+				auto next = e.target;
+				auto ncost = dist[cur] + e.cost;
+				if (dist[next] > ncost) {
+					dist[next] = ncost;
 					if (v[next]) continue;
 					v[next] = 1; q.push(next);
 				}
 			}
 		}
-		for(int i = 0; i < n; i++) pi[i] = dist[i];
+		for (int i = 0; i < n; i++) pi[i] = dist[i];
 		return true;
 	}
 
-	bool dijkstra(int s, int t) {
+	pair<cost_t, cap_t> AugmentShortest(int s, int e, cap_t flow_limit) {
+		auto infinite_cost = numeric_limits<cost_t>::max();
+		auto infinite_flow = numeric_limits<cap_t>::max();
 		typedef pair<cost_t, int> pq_t;
 		priority_queue<pq_t, vector<pq_t>, greater<pq_t>> pq;
-		fill(dist.begin(),dist.end(),COST_MAX);
-		memset(&from[0], -1, n*sizeof(from[0]));
-		memset(&v[0], 0, n*sizeof(v[0]));
-		dist[s] = 0;
-		mincap[s] = CAP_MAX;
-		pq.emplace(dist[s], s);
+		vector<pair<cost_t, cap_t>> dist(n, make_pair(infinite_cost, 0));
+		vector<int> from(n, -1), v(n);
+
+		if (needNormalize || (ranbefore && lastStart != s))
+			normalize(s);
+		ranbefore = true;
+		lastStart = s;
+
+		dist[s] = pair<cost_t, cap_t>(0, infinite_flow);
+		pq.emplace(dist[s].first, s);
 		while(!pq.empty()) {
-			int cur = pq.top().second; pq.pop();
+			auto cur = pq.top().second; pq.pop();
 			if (v[cur]) continue;
 			v[cur] = 1;
-			// do not relax from sink node. but continue computing shortest distances for the pi computation.
-			if (cur == t) continue;
-			for (const auto &edge : graph[cur]) {
-				int next = edge.target;
+			if (cur == e) continue;
+			for (const auto &e : graph[cur]) {
+				auto next = e.target;
 				if (v[next]) continue;
-				if (isZeroCap(edge.cap)) continue;
-				cost_t potCost = dist[cur] + edge.cost - pi[next] + pi[cur];
-				if (dist[next] <= potCost) continue;
-				dist[next] = potCost;
-				mincap[next] = min(mincap[cur], edge.cap);
-				from[next] = edge.rev;
-				pq.emplace(dist[next], next);
+				if (iszerocap(e.residual_capacity)) continue;
+				auto ncost = dist[cur].first + e.cost - pi[next] + pi[cur];
+				auto nflow = min(dist[cur].second, e.residual_capacity);
+				if (dist[next].first <= ncost) continue;
+				dist[next] = make_pair(ncost, nflow);
+				from[next] = e.revid;
+				pq.emplace(dist[next].first, next);
 			}
 		}
-		if (dist[t] == COST_MAX) return false;
-		for(int i = 0; i < n; i++) {
-			if (dist[i] == COST_MAX) continue;
-			pi[i] += dist[i];
+		/** augment the shortest path **/
+		auto p = e;
+		auto pathcost = dist[p].first + pi[p] - pi[s];
+		auto flow = dist[p].second;
+		if (iszerocap(flow)|| (flow_limit <= 0 && pathcost >= 0)) return pair<cost_t, cap_t>(0, 0);
+		if (flow_limit > 0) flow = min(flow, flow_limit);
+		/* update potential */
+		for (int i = 0; i < n; i++) {
+			if (iszerocap(dist[i].second)) continue;
+			pi[i] += dist[i].first;
 		}
-		return true;
+		while (from[p] != -1) {
+			auto nedge = from[p];
+			auto np = graph[p][nedge].target;
+			auto fedge = graph[p][nedge].revid;
+			graph[p][nedge].residual_capacity += flow;
+			graph[np][fedge].residual_capacity -= flow;
+			p = np;
+		}
+		return make_pair(pathcost * flow, flow);
 	}
 
-	pair<cap_t, cost_t> solve(int source, int sink) {
-		cap_t total_flow = 0;
+	pair<cost_t,cap_t> solve(int s, int e, cap_t flow_minimum = numeric_limits<cap_t>::max()) {
 		cost_t total_cost = 0;
-
-		while(dijkstra(source, sink)) {
-			cap_t f = mincap[sink];
-			total_flow += f;
-			for(int p = sink; p != source; ) {
-				auto &backward = graph[p][from[p]];
-				auto &forward = graph[backward.target][backward.rev];
-				forward.cap -= f;
-				backward.cap += f;
-				total_cost += forward.cost * f;
-				p = backward.target;
-			}
+		cap_t total_flow = 0;
+		for(;;) {
+			auto res = AugmentShortest(s, e, flow_minimum - total_flow);
+			if (res.second <= 0) break;
+			total_cost += res.first;
+			total_flow += res.second;
 		}
-
-		return make_pair(total_flow, total_cost);
+		return make_pair(total_cost, total_flow);
 	}
-}
-
+};
