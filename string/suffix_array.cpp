bcfc08170ac0f54b3bdd557574f12aa438836ad2
ChanMin Kim <kcm1700@gmail.com>
2015-11-01 02:10:49 +0900

    new suffix array, add more cutting (bucket count condition)

--- string/suffix_array.cpp
+++ string/suffix_array.cpp
@@ -1,108 +1,70 @@
+#include <cstdio>
+#include <numeric>
+#include <cstring>
 #include <algorithm>
-#include <memory.h>
+#include <vector>
 
 using namespace std;
-
-//// short names version;
-// input: n, in
-// output: o
-namespace SuffixArrayMin
+template<typename Ty>
+struct SuffixArray
 {
-	typedef char val_t;
-	const int MAX_N = 4096;
+	vector<Ty> in;
+	vector<int> out;
+	template<typename Pt>
+		SuffixArray(Pt begin, Pt end) : in(begin, end) {}
 
-	int n;
-	val_t in[MAX_N];
-	int o[MAX_N], t[MAX_N];
-	int r[MAX_N];
-
-	int b[MAX_N], m[MAX_N];
-	int p[MAX_N];
-
-	void build() {
-		for (int i = 0; i < n; i++) o[i] = i;
-		sort(o, o + n, [](int a,int b) { return in[a] < in[b];});
-		for(int i = 0, c = 0; i < n; i++) { b[i] = c; if (i+1 == n || in[o[i]] != in[o[i+1]]) c++; }
-		for(int h = 1; h < n; h <<= 1) {
-			for (int i = 0; i < n; i++) r[o[i]] = b[i];
-			for (int i = n-1; i>=0; i--) p[b[i]] = i;
-			for (int i = 0; i < n; i++) if (o[i] >= n-h) t[p[b[i]]++] = o[i];
-			for (int i = 0; i < n; i++) if (o[i] >= h) t[p[r[o[i]-h]]++] = o[i]-h;
-			for (int i = 0, c = 0; i < n; i++) {
-				m[i] = c;
-				if (i+1 == n) break;
-				if (b[i] != b[i+1]) c++;
-				else if(t[i+1] >= n-h || t[i] >= n-h) c++;
-				else if(r[t[i+1] + h] != r[t[i]+h]) c++;
-			}
-			memcpy(o, t, sizeof(t[0]) * n);
-			memcpy(b, m, sizeof(m[0]) * n);
-		}
-	}
-}
-
-
-//// unmanged names
-// input: n, in
-// output: out
-namespace SuffixArray
-{
-	typedef char val_t;
-	const int MAX_N = 4096;
-
-	int n;
-	val_t in[MAX_N];
-	int out[MAX_N], temp[MAX_N];
-	int pos2bckt[MAX_N];
-
-	int bckt[MAX_N], relabel[MAX_N];
-	int bpos[MAX_N];
-
-	void build() {
+	vector<int> build() {
+		int n = (int)in.size(), c = 0;
+		vector<int> temp(n), pos2bckt(n), bckt(n), bpos(n);
+		out.resize(n);
 		for (int i = 0; i < n; i++) out[i] = i;
-		sort(out, out + n, [](int a,int bckt) { return in[a] < in[bckt];});
-		for(int i = 0, c = 0; i < n; i++) { bckt[i] = c; if (i+1 == n || in[out[i]] != in[out[i+1]]) c++; }
-		for(int h = 1; h < n; h <<= 1) {
+		sort(out.begin(), out.end(), [&](int a,int b) { return in[a] < in[b];});
+		for(int i = 0; i < n; i++) { bckt[i] = c; if (i+1 == n || in[out[i]] != in[out[i+1]]) c++; }
+		for(int h = 1; h < n && c < n; h <<= 1) {
 			for (int i = 0; i < n; i++) pos2bckt[out[i]] = bckt[i];
 			for (int i = n-1; i>=0; i--) bpos[bckt[i]] = i;
 			for (int i = 0; i < n; i++) if (out[i] >= n-h) temp[bpos[bckt[i]]++] = out[i];
 			for (int i = 0; i < n; i++) if (out[i] >= h) temp[bpos[pos2bckt[out[i]-h]]++] = out[i]-h;
-			for (int i = 0, c = 0; i < n; i++) {
-				relabel[i] = c;
-				if (i+1 == n) break;
-				if (bckt[i] != bckt[i+1]) c++;
-				else if(temp[i+1] >= n-h || temp[i] >= n-h) c++;
-				else if(pos2bckt[temp[i+1] + h] != pos2bckt[temp[i]+h]) c++;
+			c = 0;
+			for (int i = 0; i + 1 < n; i++) {
+				int a = (bckt[i] != bckt[i+1]) || (temp[i] >= n-h) || pos2bckt[temp[i+1]+h] != pos2bckt[temp[i]+h];
+				bckt[i] = c;
+				c += a;
 			}
-			memcpy(out, temp, sizeof(temp[0]) * n);
-			memcpy(bckt, relabel, sizeof(relabel[0]) * n);
+			bckt[n-1] = c++;
+			temp.swap(out);
 		}
+		return out;
 	}
-}
 
+	vector<int> lcparray() {
+		if (in.size() != out.size()) build();
+		int n = (int)in.size();
+		vector<int> rank(n); // temporary
+		vector<int> height(n-1); // output lcp array
 
-#include <string.h>
-
-int main()
-{
-	const char *str = "abracadabra";
-	SuffixArray::n = strlen(str);
-	SuffixArrayMin::n = strlen(str);
-	strcpy(SuffixArray::in, str);
-	strcpy(SuffixArrayMin::in, str);
-
-	SuffixArray::build();
-	SuffixArrayMin::build();
-
-	printf("11,8,1,4,6,9,2,5,7,10,3,\n");
-	for(int i = 0; i < SuffixArray::n; i++) {
-		printf("%d,", SuffixArray::out[i]+1);
+		for(int i = 0; i < n; i ++) rank[out[i]] = i;
+		int h = 0;
+		for(int i = 0; i < n; i ++) {
+			if(rank[i] == 0) continue;
+			int j = out[rank[i]-1];
+			while(i+h < n && j+h < n && in[i+h] == in[j+h]) h++;
+			height[rank[i]-1] = h;
+			if(h > 0) h--;
+		}
+		return height;
 	}
-	printf("\n");
-	for(int i = 0; i < SuffixArrayMin::n; i++) {
-		printf("%d,", SuffixArrayMin::o[i]+1);
-	}
-	printf("\n");
+};
 
+
+#include <cstdlib>
+#include <ctime>
+
+int main(){
+	string s;
+	for (int i = 0; i < 1048576; i++) s.push_back(rand()%64+48);
+	SuffixArray<char> sa(s.begin(), s.end());
+	sa.lcparray();
+	printf("%f\n", (double) (clock()*1000.0/CLOCKS_PER_SEC));
 	return 0;
 }
