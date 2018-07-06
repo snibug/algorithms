861ad70329cc13714fd45e838513171ac8cd7caa
ChanMin Kim <kcm1700@gmail.com>
2018-07-07 02:08:56 +0900

    LIS 간략화

--- longest_increasing_subsequence.cpp
+++ longest_increasing_subsequence.cpp
@@ -12,21 +12,19 @@ vector<int> solveLIS(const vector<T> &dat, bool strict = true) {
   vector<pair<T, int>> st;
   st.reserve(n);
   vector<int> prev(n);
-  pair<int, int> ans = make_pair(0, 0);
   for (int i = 0; i < n; i++) {
     auto I = lower_bound(st.begin(), st.end(), make_pair(dat[i], strict ? -1 : n));
-    int index = I - st.begin();
-    ans = max(ans, make_pair(index + 1, i));
-    if (index == 0) prev[i] = -1;
-    else prev[i] = st[index - 1].second;
+    int index = (int)(I - st.begin());
+    prev[i] = ((index == 0) ? -1 : st[index - 1].second);
     if (I == st.end()) st.emplace_back(dat[i], i);
     else *I = make_pair(dat[i], i);
   }
-  vector<int> sequence(ans.first);
-  int p = ans.second;
-  for (int i = ans.first - 1; p >= 0; i--) {
+  vector<int> sequence(st.size());
+  int p = st.empty() ? -1 : st.back().second;
+  for (int i = (int)sequence.size() - 1; p >= 0; i--) {
     sequence[i] = p;
     p = prev[p];
   }
   return sequence;
 }
+
