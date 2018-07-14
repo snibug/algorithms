b23e108a22e2f5c93a4b1368babddbdbb6e59e32
ChanMin Kim <kcm1700@gmail.com>
2018-07-14 19:02:38 +0900

    mcmf 알아보기 쉽게 수정

--- graph/mcmf.cpp
+++ graph/mcmf.cpp
@@ -37,19 +37,21 @@ struct MinCostFlow
 	int n;
 	vector<vector<edge>> graph;
 	vector<cost_t> pi;
-	bool needNormalize, ranbefore;
-	int lastStart;
+	bool needNormalize;
 
-	MinCostFlow(int n) : graph(n), n(n), pi(n, 0), needNormalize(false), ranbefore(false) {}
+	MinCostFlow(int n) : graph(n), n(n), pi(n, 0), needNormalize(false) {}
 	void addEdge(int s, int e, cost_t cost, cap_t cap)
 	{
 		if (s == e) return;
 		edge forward={e, cost, cap, cap, graph[e].size()};
 		edge backward={s, -cost, 0, 0, graph[s].size()};
-		if (cost < 0 || ranbefore) needNormalize = true;
+		if (cost < 0) {
+			needNormalize = true;
+		}
 		graph[s].emplace_back(forward);
 		graph[e].emplace_back(backward);
 	}
+
 	bool normalize(int s) {
 		auto infinite_cost = numeric_limits<cost_t>::max();
 		vector<cost_t> dist(n, infinite_cost);
@@ -76,7 +78,7 @@ struct MinCostFlow
 		return true;
 	}
 
-	pair<cost_t, cap_t> AugmentShortest(int s, int e, cap_t flow_limit) {
+	pair<cost_t, cap_t> augmentShortest(int s, int e, cap_t flow_goal) {
 		auto infinite_cost = numeric_limits<cost_t>::max();
 		auto infinite_flow = numeric_limits<cap_t>::max();
 		typedef pair<cost_t, int> pq_t;
@@ -84,10 +86,10 @@ struct MinCostFlow
 		vector<pair<cost_t, cap_t>> dist(n, make_pair(infinite_cost, 0));
 		vector<int> from(n, -1), v(n);
 
-		if (needNormalize || (ranbefore && lastStart != s))
+		if (needNormalize) {
 			normalize(s);
-		ranbefore = true;
-		lastStart = s;
+			needNormalize = false;
+		}
 
 		dist[s] = pair<cost_t, cap_t>(0, infinite_flow);
 		pq.emplace(dist[s].first, s);
@@ -95,46 +97,49 @@ struct MinCostFlow
 			auto cur = pq.top().second; pq.pop();
 			if (v[cur]) continue;
 			v[cur] = 1;
-			if (cur == e) continue;
-			for (const auto &e : graph[cur]) {
-				auto next = e.target;
+			for (const auto &edge : graph[cur]) {
+				auto next = edge.target;
 				if (v[next]) continue;
-				if (iszerocap(e.residual_capacity)) continue;
-				auto ncost = dist[cur].first + e.cost - pi[next] + pi[cur];
-				auto nflow = min(dist[cur].second, e.residual_capacity);
+				if (iszerocap(edge.residual_capacity)) continue;
+				auto ncost = dist[cur].first + edge.cost - pi[next] + pi[cur];
+				auto nflow = min(dist[cur].second, edge.residual_capacity);
 				if (dist[next].first <= ncost) continue;
 				dist[next] = make_pair(ncost, nflow);
-				from[next] = e.revid;
+				from[next] = edge.revid;
 				pq.emplace(dist[next].first, next);
 			}
 		}
 		/** augment the shortest path **/
-		auto p = e;
-		auto pathcost = dist[p].first + pi[p] - pi[s];
-		auto flow = dist[p].second;
-		if (iszerocap(flow)|| (flow_limit <= 0 && pathcost >= 0)) return pair<cost_t, cap_t>(0, 0);
-		if (flow_limit > 0) flow = min(flow, flow_limit);
-		/* update potential */
+		/* update potential, dist[i].first becomes invalid */
 		for (int i = 0; i < n; i++) {
 			if (iszerocap(dist[i].second)) continue;
 			pi[i] += dist[i].first;
 		}
-		while (from[p] != -1) {
-			auto nedge = from[p];
-			auto np = graph[p][nedge].target;
-			auto fedge = graph[p][nedge].revid;
-			graph[p][nedge].residual_capacity += flow;
-			graph[np][fedge].residual_capacity -= flow;
-			p = np;
+		auto flow = dist[e].second;
+		// e is unreachable
+		if (iszerocap(flow)) return make_pair(0, 0);
+		// reduced cost of augment path is 0, actual cost is potential difference
+		auto pathcost = pi[e] - pi[s];
+		// if it costs, we want minimum flow
+		if (pathcost >= 0) flow = min(flow, flow_goal);
+		if (flow <= 0 || iszerocap(flow)) return make_pair(0, 0);
+
+		for (auto cur = e; from[cur] != -1; ) {
+			auto back_edge = from[cur];
+			auto prev = graph[cur][back_edge].target;
+			auto forward_edge = graph[cur][back_edge].revid;
+			graph[cur][back_edge].residual_capacity += flow;
+			graph[prev][forward_edge].residual_capacity -= flow;
+			cur = prev;
 		}
 		return make_pair(pathcost * flow, flow);
 	}
 
-	pair<cost_t,cap_t> solve(int s, int e, cap_t flow_minimum = numeric_limits<cap_t>::max()) {
+	pair<cost_t,cap_t> solve(int s, int e, cap_t flow_goal = numeric_limits<cap_t>::max()) {
 		cost_t total_cost = 0;
 		cap_t total_flow = 0;
 		for(;;) {
-			auto res = AugmentShortest(s, e, flow_minimum - total_flow);
+			auto res = augmentShortest(s, e, flow_goal - total_flow);
 			if (res.second <= 0) break;
 			total_cost += res.first;
 			total_flow += res.second;
