750099f81528039c7c5323d9906e9ad45cdb9863
ChanMin Kim <kcm1700@gmail.com>
2018-07-16 13:42:14 +0900

    약간 수정

--- math/FastLinearRecurrence.cpp
+++ math/FastLinearRecurrence.cpp
@@ -17,6 +17,7 @@
 #include <thread>
 #include <tuple>
 #include <utility>
+#include <cassert>
 
 using namespace std;
 
@@ -72,7 +73,6 @@ struct FastLinearRecurrence {
         Poly &operator =(Poly &&) = default;
     };
 
-    Poly p;
     int m;
 
     vector<Poly> powerJumps; // some pre-calculated jumps
@@ -82,16 +82,19 @@ struct FastLinearRecurrence {
      * a[n] = sum(a[n-m + i] * c[i]) + d
      * O(C(m) (log m)^2)
      * O(m^2 log m) if convolution is naive algorithm
+     * Assumption: m > 0
      */
-    FastLinearRecurrence(vector<num_t> orig_c, num_t d) : p(move(orig_c), d, 1) {
-        m = (int)p.c.size();
+    FastLinearRecurrence(vector<num_t> orig_c, num_t d) {
+        Poly q(move(orig_c), d, 1);
+        m = (int)q.c.size();
         powerJumps.reserve(65);
-        Poly q = p;
         powerJumps.emplace_back(q);
-        for (int jump_index = 1; (1 << jump_index) < m; jump_index++) {
+        while (q.jump * 2 <= m) {
             q = double_jump(q);
             powerJumps.emplace_back(q);
         }
+        // collected 2^i <= m.
+        // therefore m <= sum(jump)
     }
 
     vector<num_t> convolution(const vector<num_t> &a, const vector<num_t> &b) const {
@@ -105,12 +108,17 @@ struct FastLinearRecurrence {
         return result;
     }
 
-    /* Supported length [0, m + least_power_2(m)) or [0, sum(powerJump.jump)) or [0, 2m)
+    /* Make size <= m
+     *
+     * Supported length [0, m + sum(powerJump.jump)] or [0, 2m]
+     * because m <= sum(powerJump.jump)
+     *
      * O(C(m) log m) where C(m) = convolution time complexity,
      * O(m^2) if convolution is naive algorithm */
     Poly reduce(Poly p) const {
         auto &coefficient = p.c;
         if ((int)coefficient.size() <= m) {
+            // already reduced size
             return p;
         }
         for (int jump_index = (int)powerJumps.size() - 1; jump_index >= 0; jump_index--) {
@@ -118,9 +126,10 @@ struct FastLinearRecurrence {
             int start = m + (1 << jump_index) - 1;
             if (start >= (int)coefficient.size()) continue;
             const auto &part = convolution(
-                    vector<num_t>(coefficient.begin() + start, coefficient.end()),
-                    jump.c
+                    vector<num_t>(coefficient.begin() + start, coefficient.end()), /* length <= (1<<jump_index) */
+                    jump.c /* length = m */
             );
+            /* part.length <= m + (1<<jump_index) - 1 = start */
             for (int i = start; i < (int)coefficient.size(); i++) {
                 p.d = p.d + jump.d * coefficient[i];
             }
@@ -136,7 +145,6 @@ struct FastLinearRecurrence {
 
     /* O(C(m) log m) */
     Poly double_jump(const Poly &a) const {
-        // a.jump <= b.jump
         int offset = a.jump;
         Poly result(vector<num_t>(a.c.size() + offset), a.d, a.jump);
         copy(a.c.begin(), a.c.end(), result.c.begin() + offset);
@@ -145,18 +153,20 @@ struct FastLinearRecurrence {
 
     /* O(C(m) log m log jump) */
     Poly calculate(long long jump) const {
-        if (jump - powerJumps.back().jump < m) {
+        if (jump - powerJumps.back().jump <= m) {
             for (int jump_index = (int)powerJumps.size() - 1; jump_index >= 0; jump_index--) {
                 const Poly &p = powerJumps[jump_index];
                 if (p.jump == jump) {
                     return p;
                 }
                 if (p.jump < jump) {
-                    auto offset = static_cast<int>(jump - p.jump);
-                    Poly result(vector<num_t>(p.c.size() + offset), p.d, p.jump);
+                    auto offset = static_cast<int>(jump - p.jump); // <= m
+                    Poly result(vector<num_t>(p.c.size() + offset), p.d, p.jump); // <= 2m
                     copy(p.c.begin(), p.c.end(), result.c.begin() + offset);
                     return reduce(move(result));
                 }
+                // jump - p.jump < 0, and the largest jump <= m, so skipping adds m/2 or less
+                // therefore jump - p.jump < m
             }
             // it means 0 or less jump
             return Poly();
@@ -213,6 +223,39 @@ void test1() {
     for (int i = 0; i < 1000; i++) {
         modint new_val =
                 modint(2) * seed[seed.size() - 1] +
+                modint(3);
+        seed.push_back(new_val);
+    }
+    for (int i = 0; i < 10; i++) {
+        modint t = flr.calculate(i, realseed);
+        printf("should match: %d %d\n", seed[i].val, t.val);
+        if (seed[i] != t) {
+            printf("mismatch!!\n");
+        }
+    }
+    {
+        // some large number
+        modint t = flr.calculate(800, realseed);
+        printf("should match: %d %d\n", seed[800].val, t.val);
+        if (seed[800] != t) {
+            printf("mismatch!!\n");
+        }
+    }
+}
+
+void test2() {
+    // a(n) = 2 a(n-2) + 7 a(n-1) + 3
+    FastLinearRecurrence flr(
+            {modint(2), modint(7)},
+            modint(3)
+    );
+    // a(0) = 1, a(1) = 2
+    vector<modint> seed = {modint(1), modint(2)};
+    vector<modint> realseed = seed;
+    for (int i = 0; i < 1000; i++) {
+        modint new_val =
+                modint(2) * seed[seed.size() - 2] +
+                modint(7) * seed[seed.size() - 1] +
                 modint(3);
         seed.push_back(new_val);
     }
@@ -304,6 +347,7 @@ void test4() {
 
 int main() {
     test1();
+    test2();
     test3();
     test4();
     return 0;
